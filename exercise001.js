1be635bb032d8c9197f33628d78321919ae9286e
Ahmed <imaneben8@gmail.com>
2023-06-11 01:03:05 +0100

    Convert your .then().catch() into Async & Await

--- exercise001.js
+++ exercise001.js
@@ -7,16 +7,26 @@ const promise = new Promise((resolve, reject) => {
     setTimeout(() => resolve("done"), 2000);
   } else {
     // Failure
-    setTimeout(() => reject("error"), 2000);
+    setTimeout(() => reject("flababada"), 2000);
   }
 });
 
 // Your solution(s) to exercise001 here!
 
-promise
-  .then((resolve) => {
-    console.log(`Yay! Promise resolved with response: `);
-  })
-  .catch((reject) => {
-    console.log(`Boo. Promise rejected with response: `);
-  });
+// promise
+//   .then((resolve) => {
+//     console.log(`Yay! Promise resolved with response: `);
+//   })
+//   .catch((reject) => {
+//     console.log(`Boo. Promise rejected with response: `);
+//   });
+
+async function getPromiseResponse() {
+  try {
+    const result = await promise;
+    console.log(`Yay! Promise resolved with response: ${result}`);
+  } catch (error) {
+    console.log(`Boo. Promise rejected with response: ${error}`);
+  }
+}
+getPromiseResponse();
