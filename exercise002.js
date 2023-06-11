17191cb574ea582ea77191552bcff5ddb3cb8ab1
Ahmed <imaneben8@gmail.com>
2023-06-11 23:43:46 +0100

    Try calling other APIs

--- exercise002.js
+++ exercise002.js
@@ -1,12 +1,22 @@
 import fetch from "node-fetch";
 
-const jsonTypicode = "https://jsonplaceholder.typicode.com/todos/1";
+const jsonTypicode = "https://cat-fact.herokuapp.com/facts/random";
 
-const fetchData = (apiEndPoint) => {
-  fetch(apiEndPoint)
-    .then((response) => response.json())
-    .then((json) => console.log(json))
-    .catch((error) => console.log(error));
-};
+// const fetchData = (apiEndPoint) => {
+//   fetch(apiEndPoint)
+//     .then((response) => response.json())
+//     .then((json) => console.log(json))
+//     .catch((error) => console.log(error));
+// };
 
+// fetchData(jsonTypicode);
+async function fetchData(apiEndPoint) {
+  try {
+    const response = await fetch(apiEndPoint);
+    const json = await response.json();
+    console.log(json);
+  } catch (error) {
+    console.log(error);
+  }
+}
 fetchData(jsonTypicode);
